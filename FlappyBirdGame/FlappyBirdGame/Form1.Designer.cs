9e65b85ca6f30d5b1835842872010464fe573ac3
nergisalbayrak <nrgsalbayrak2005@gmail.com>
2025-09-30 21:31:36 +0300

    Add files via upload

---
+++ FlappyBirdGame/FlappyBirdGame/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FlappyBirdGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox_Bird = new System.Windows.Forms.PictureBox();
+            this.pictureBox_top = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Bottom = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Ground = new System.Windows.Forms.PictureBox();
+            this.timer_GameControl = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SCORE:";
+            // 
+            // pictureBox_Bird
+            // 
+            this.pictureBox_Bird.Image = global::FlappyBirdGame.Properties.Resources.bird;
+            this.pictureBox_Bird.Location = new System.Drawing.Point(58, 156);
+            this.pictureBox_Bird.Name = "pictureBox_Bird";
+            this.pictureBox_Bird.Size = new System.Drawing.Size(80, 60);
+            this.pictureBox_Bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox_Bird.TabIndex = 8;
+            this.pictureBox_Bird.TabStop = false;
+            // 
+            // pictureBox_top
+            // 
+            this.pictureBox_top.Image = global::FlappyBirdGame.Properties.Resources.pipedown;
+            this.pictureBox_top.Location = new System.Drawing.Point(820, 0);
+            this.pictureBox_top.Name = "pictureBox_top";
+            this.pictureBox_top.Size = new System.Drawing.Size(158, 216);
+            this.pictureBox_top.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_top.TabIndex = 7;
+            this.pictureBox_top.TabStop = false;
+            // 
+            // pictureBox_Bottom
+            // 
+            this.pictureBox_Bottom.Image = global::FlappyBirdGame.Properties.Resources.pipe;
+            this.pictureBox_Bottom.Location = new System.Drawing.Point(443, 407);
+            this.pictureBox_Bottom.Name = "pictureBox_Bottom";
+            this.pictureBox_Bottom.Size = new System.Drawing.Size(158, 272);
+            this.pictureBox_Bottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Bottom.TabIndex = 6;
+            this.pictureBox_Bottom.TabStop = false;
+            // 
+            // pictureBox_Ground
+            // 
+            this.pictureBox_Ground.Image = global::FlappyBirdGame.Properties.Resources.ground;
+            this.pictureBox_Ground.Location = new System.Drawing.Point(-3, 614);
+            this.pictureBox_Ground.Name = "pictureBox_Ground";
+            this.pictureBox_Ground.Size = new System.Drawing.Size(1352, 273);
+            this.pictureBox_Ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Ground.TabIndex = 3;
+            this.pictureBox_Ground.TabStop = false;
+            // 
+            // timer_GameControl
+            // 
+            this.timer_GameControl.Enabled = true;
+            this.timer_GameControl.Interval = 20;
+            this.timer_GameControl.Tick += new System.EventHandler(this.GameTimer);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(1344, 727);
+            this.Controls.Add(this.pictureBox_Bird);
+            this.Controls.Add(this.pictureBox_top);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox_Ground);
+            this.Controls.Add(this.pictureBox_Bottom);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.game_Down);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.game_Up);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox_Ground;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_Bottom;
+        private System.Windows.Forms.PictureBox pictureBox_top;
+        private System.Windows.Forms.PictureBox pictureBox_Bird;
+        private System.Windows.Forms.Timer timer_GameControl;
+    }
+}
+
---
+++ FlappyBirdGame/FlappyBirdGame/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBirdGame
+{
+    public partial class Form1 : Form
+    {
+        int pipeSpeed = 8;
+        int gravity = 15;
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GameTimer(object sender, EventArgs e)
+        {
+            pictureBox_Bird.Top += gravity;
+            pictureBox_Bottom.Left -= pipeSpeed;
+            pictureBox_top.Left -= pipeSpeed;
+            label1.Text = "SCORE" + score;
+
+            if (pictureBox_Bottom.Left < -150)
+            {
+                pictureBox_Bottom.Left = 800;
+                score++;
+            }
+            if (pictureBox_top.Left < -180)
+            {
+                pictureBox_top.Left = 950;
+                score++;
+            }
+            if (pictureBox_Bird.Bounds.IntersectsWith(pictureBox_Bottom.Bounds) ||
+                pictureBox_Bird.Bounds.IntersectsWith(pictureBox_top.Bounds) ||
+                pictureBox_Bird.Bounds.IntersectsWith(pictureBox_Ground.Bounds) || pictureBox_Bird.Top < -25)
+
+            {
+                EndGame();
+
+            }
+        }
+            public void EndGame()
+            {
+            timer_GameControl.Stop();
+            label1.Text = "GAME OVER !";
+
+            }
+
+        private void game_Down(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = -15;
+            }
+        }
+
+        private void game_Up(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = 15;
+            }
+        }
+    }
+    }
+
